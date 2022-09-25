0bbc48952fb33b4ac14f83f80aee7824db1e0503
Haddemine <Haddemin1234@gmail.com>
2022-09-25 12:25:51 +0000

    k

--- apis/admin.py
+++ apis/admin.py
@@ -10,4 +10,30 @@ class ClientAdmin(admin.ModelAdmin):
 class ClientAdminapp(admin.ModelAdmin):
     search_fields=['nom']
 
+@admin.register(Ppm)
+class ClientAdminppm(admin.ModelAdmin):
+    search_fields=['nom']
 
+@admin.register(Contrat)
+class ClientAdmincontrat(admin.ModelAdmin):
+    search_fields=['nom']
+
+@admin.register(Conge)
+class ClientAdminconge(admin.ModelAdmin):
+    search_fields=['nom']
+
+@admin.register(Contrat)
+class ClientAdmincontrat(admin.ModelAdmin):
+    search_fields=['nom']
+
+@admin.register(Comptabilite)
+class ClientAdmincomptabilite(admin.ModelAdmin):
+    search_fields=['nom']
+
+@admin.register(Documents)
+class ClientAdmindocument(admin.ModelAdmin):
+    search_fields=['nom']
+
+@admin.register(Ordre_mission)
+class ClientAdminorder(admin.ModelAdmin):
+    search_fields=['nom']
