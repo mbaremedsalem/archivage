1f9a092f1b9ace646ea545f883c672b505833242
Haddemine <Haddemin1234@gmail.com>
2022-09-25 12:35:59 +0000

    k

--- apis/admin.py
+++ apis/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
 from apis.models import  *
+from django.contrib import admin
 
 
 @admin.register(Employee)
@@ -12,10 +12,3 @@ class ClientAdmin(admin.ModelAdmin):
 admin.site.register(Ppm)
 admin.site.register(Appel_offre)
 
-
-admin.site.register(Contrat)
-admin.site.register(Ordre_mission)
-admin.site.register(Comptabilite)
-admin.site.register(Documents)
-admin.site.register(Contrat)
-admin.site.register(Conge)
