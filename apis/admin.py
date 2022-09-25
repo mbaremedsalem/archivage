cf8c5223da3e93f1cd5c6f7a3fad96928c9c6d5c
Haddemine <Haddemin1234@gmail.com>
2022-09-25 12:30:43 +0000

    k

--- apis/admin.py
+++ apis/admin.py
@@ -6,34 +6,15 @@ from apis.models import  *
 class ClientAdmin(admin.ModelAdmin):
     search_fields=['nom']
 
-@admin.register(Appel_offre)
-class ClientAdminapp(admin.ModelAdmin):
-    search_fields=['nom']
 
-@admin.register(Ppm)
-class ClientAdminppm(admin.ModelAdmin):
-    search_fields=['nom']
 
-@admin.register(Contrat)
-class ClientAdmincontrat(admin.ModelAdmin):
-    search_fields=['nom']
+admin.site.register(Ppm)
+admin.site.register(Appel_offre)
 
-@admin.register(Conge)
-class ClientAdminconge(admin.ModelAdmin):
-    search_fields=['nom']
 
-@admin.register(Contrat)
-class ClientAdmincontrat(admin.ModelAdmin):
-    search_fields=['nom']
-
-@admin.register(Comptabilite)
-class ClientAdmincomptabilite(admin.ModelAdmin):
-    search_fields=['nom']
-
-@admin.register(Documents)
-class ClientAdmindocument(admin.ModelAdmin):
-    search_fields=['nom']
-
-@admin.register(Ordre_mission)
-class ClientAdminorder(admin.ModelAdmin):
-    search_fields=['nom']
+admin.site.register(Contrat)
+admin.site.register(Ordre_mission)
+admin.site.register(Comptabilite)
+admin.site.register(Documents)
+admin.site.register(Contrat)
+admin.site.register(Conge)
